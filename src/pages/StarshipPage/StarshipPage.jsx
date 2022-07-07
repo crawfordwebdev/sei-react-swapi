cffc7b950309c630515a775255a69a69ffdd1240
Michael Crawford <crawfordwebdev@users.noreply.github.com>
2022-07-07 14:09:06 -0600

    adds view pilot functionality

--- src/pages/StarshipPage/StarshipPage.jsx
+++ src/pages/StarshipPage/StarshipPage.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react'
-import { getStarship } from '../../services/sw-api'
+import { getStarship, getPilots } from '../../services/sw-api'
 import { Link } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
 
-
 const StarshipPage = () => {
     const [starshipDetails, setStarshipDetails] = useState({})
+    const [pilots, setPilots] = useState([])
+
     const location = useLocation()
     
     useEffect(() => {
@@ -17,6 +18,15 @@ const StarshipPage = () => {
       
     }, [location.state.starship.url])
 
+    useEffect(() => {
+      const fetchDetails = async () => {
+        // console.log("location.state.starship.pilots", location.state.starship.pilots)
+        const pilots = await getPilots(location.state.starship.pilots)
+        setPilots(pilots)
+      }
+      fetchDetails()
+    }, [location.state.starship.pilots])
+
     
   return (
     <>
@@ -27,6 +37,20 @@ const StarshipPage = () => {
           <div><span>Model:</span><span>{starshipDetails.model}</span></div>
           <div><Link to='/'>Return</Link></div>
         </div>
+        {pilots ?
+          <>
+          <h3>Pilot List</h3>
+          {pilots.map((pilot, idx) => 
+            <div key={idx}>
+              {pilot.name}
+            </div>
+          )}
+          </>
+        :
+          <>
+            <p>No Pilots</p>
+          </>
+        }
       </>
       :
       <>
--- src/services/sw-api.js
+++ src/services/sw-api.js
@@ -9,3 +9,14 @@ export async function getStarship(apiUrl) {
   const res = await fetch(apiUrl)
   return res.json()
 }
+
+export async function getPilots(urls) {
+  const promises = urls.map(url => 
+    fetch(url)
+    .then(
+      res => res.json()
+      )
+    )
+  const pilotObjects = await Promise.all(promises)
+  return pilotObjects
+}
