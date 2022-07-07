61d8ad4d1908d4127cde02eca4af120411e9254b
Michael Crawford <crawfordwebdev@users.noreply.github.com>
2022-07-07 11:56:38 -0600

    adds starship-link class

--- src/pages/StarshipList/StarshipList.jsx
+++ src/pages/StarshipList/StarshipList.jsx
@@ -16,12 +16,11 @@ const StarshipList = () => {
 
   return (
     <div>
-      <h3>Starship List</h3>
       { starships ?
       <>
         <div className="starship-container">
           {starships.map(starship =>
-            <Link key={starship.name} state={{ starship }} to='/starship'>
+            <Link className="starship-link" key={starship.name} state={{ starship }} to='/starship'>
               <div className="starship-div">
                 {starship.name}
               </div>
